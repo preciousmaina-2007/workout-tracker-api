235ad14b3bc73d7b71480847752391ed57bb453b
precious <precious.maina@student.moringaschool.com>
2026-07-17 12:24:22 +0300

    create exercise workout and workoutexercises models

--- server/models.py
+++ server/models.py
@@ -1,3 +1,42 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String, nullable=False)
+
+    category = db.Column(db.String, nullable=False)
+
+    equipment_needed = db.Column(db.Boolean, default=False)
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    date = db.Column(db.Date, nullable=False)
+
+    duration_minutes = db.Column(db.Integer, nullable=False)
+
+    notes = db.Column(db.Text)    
+
+class WorkoutExercise(db.Model):
+    __tablename__ = "workout_exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    workout_id = db.Column(db.Integer,
+                           db.ForeignKey("workouts.id"))
+
+    exercise_id = db.Column(db.Integer,
+                            db.ForeignKey("exercises.id"))
+
+    reps = db.Column(db.Integer)
+
+    sets = db.Column(db.Integer)
+
+    duration_seconds = db.Column(db.Integer)
